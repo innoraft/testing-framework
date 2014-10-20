1024386f4b33ac486a38c7ca2e68e24c113fd733
aman <aman.goel2452@gmail.com>
2014-10-20 17:24:34 +0530

    first commit

---
+++ Test Local site on browserStack/TestLocalSite.java
@@ -0,0 +1,95 @@
+// First connect the local server with browserstack server for test local site on browserstack.
+
+package BrowserStack;
+
+import java.net.URL;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Local_Test {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+	// Describe the capabilities for test.
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("browser", "chrome");
+        caps.setCapability("browser_version", "18");
+        caps.setCapability("os", "Windows");
+        caps.setCapability("os_version", "7");
+        caps.setCapability("browserstack.debug", "true");
+        caps.setCapability("build", "version1");
+        caps.setCapability("project", "Local_Test");
+        caps.setCapability("browserstack.local", "true");
+
+	// Test will run on browserStack by specifying the username and automate key.
+        driver = new RemoteWebDriver(new URL("http://" + USERNAME + ":" + AUTOMATE_KEY + "@hub.browserstack.com/wd/hub"), caps);
+        baseUrl = "http://localhost/";
+	// How much time driver will wait for element to be loaded.
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testUntitled() throws Exception {
+        driver.get(baseUrl + "/site-name/");
+        driver.findElement(By.id("edit-name")).clear();
+        driver.findElement(By.id("edit-name")).sendKeys("username");
+        driver.findElement(By.id("edit-pass")).clear();
+        driver.findElement(By.id("edit-pass")).sendKeys("password");
+        driver.findElement(By.id("edit-submit")).click();
+        driver.findElement(By.linkText("Developer Timesheet")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
